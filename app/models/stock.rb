f6004f8230672a2864a68915123ee5de9b4a9a3d
Rails Udemy Course <cesbecer@gmail.com>
2016-04-16 01:16:40 +0000

    List stocks and many to many associations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,10 @@ class Stock < ActiveRecord::Base
   # tied to any objects or object lifecycle, we need to be able to use them 
   #without having any instances of a stock.
   
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
